633b54efb4801fa806787de7ed40e915d97a092f
chnoumanejaz <noumanejaz92@gmail.com>
2024-03-30 03:31:23 +0500

    colors filter options added

--- app/page.tsx
+++ app/page.tsx
@@ -13,11 +13,37 @@ import { ChevronDown, Filter } from 'lucide-react';
 import { useState } from 'react';
 import { Product } from './db';
 import ProductCard from '@/components/products/ProductCard';
+import ProductSkeleton from '@/components/products/ProductSkeleton';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
 
 const SORT_OPTIONS = [
   { name: 'None', value: 'none' },
   { name: 'Price: Low to High', value: 'price-asc' },
   { name: 'Price: High to Low', value: 'price-desc' },
+] as const;
+
+const COLOR_FILTERS = {
+  id: 'color',
+  name: 'Color',
+  options: [
+    { value: 'white', label: 'White' },
+    { value: 'beige', label: 'Beige' },
+    { value: 'blue', label: 'Blue' },
+    { value: 'green', label: 'Green' },
+    { value: 'purple', label: 'Purple' },
+  ] as const,
+};
+
+const SUB_CATEGORIES = [
+  { name: 'T-Shirts', selected: true, href: '#' },
+  { name: 'Hoodies', selected: false, href: '#' },
+  { name: 'Sweatshirts', selected: false, href: '#' },
+  { name: 'Accessories', selected: false, href: '#' },
 ] as const;
 
 export default function Home() {
@@ -93,14 +119,59 @@ export default function Home() {
       <section className="pb-24 pt-6 ">
         <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4">
           {/* filters */}
-          <div></div>
+          <div className="hidden lg:block ">
+            <ul className="space-y-4 border-b border-gray-200 pb-6 text-sm font-medium text-gray-900">
+              {SUB_CATEGORIES.map(subCategory => (
+                <li key={subCategory.name}>
+                  <button
+                    className="disabled:cursor-not-allowed disabled:opacity-60 "
+                    disabled={!subCategory.selected}>
+                    {subCategory.name}
+                  </button>
+                </li>
+              ))}
+            </ul>
+
+            <Accordion type="multiple" className="animate-none">
+              {/* Color filter */}
+
+              <AccordionItem value="color">
+                <AccordionTrigger className="py-2 text-sm text-gray-400 hover:text-gray-500">
+                  <span className="font-medium text-gray-900 ">Color</span>
+                </AccordionTrigger>
+
+                <AccordionContent className="pt-3 animate-none">
+                  <ul className="space-y-4">
+                    {COLOR_FILTERS.options.map((option, idx) => (
+                      <li key={option.value} className="flex items-center">
+                        <input
+                          type="checkbox"
+                          id={`color-${idx}`}
+                          className="h-4 w-4 rounded border-gray-300 text-indigo-600 cursor-pointer focus:ring-indigo-500"
+                        />
+                        <label
+                          htmlFor={`color-${idx}`}
+                          className="ml-3 text-sm text-gray-600  cursor-pointer">
+                          {option.label}
+                        </label>
+                      </li>
+                    ))}
+                  </ul>
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+          </div>
 
           {/* Products  */}
 
           <ul className="lg:col-span-3 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-            {products?.map(product => (
-              <ProductCard product={product.metadata!} key={product.id} />
-            ))}
+            {!products
+              ? new Array(12)
+                  .fill(null)
+                  .map((_, idx) => <ProductSkeleton key={idx} />)
+              : products.map(product => (
+                  <ProductCard product={product.metadata!} key={product.id} />
+                ))}
           </ul>
         </div>
       </section>
