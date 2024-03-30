24e7d40fbfbda38308123c69e0014e82dbf502f4
chnoumanejaz <noumanejaz92@gmail.com>
2024-03-30 16:20:03 +0500

    site url added

--- app/page.tsx
+++ app/page.tsx
@@ -88,7 +88,7 @@ export default function Home() {
     queryKey: ['products'],
     queryFn: async () => {
       const { data } = await axios.post<QueryResult<Product>[]>(
-        'http://localhost:3000/api/products',
+        'https://advance-filtering-nouman.netlify.app/api/products',
         {
           filter: {
             sort: filter.sort,
