9639f820dd15bb4bc50f45821a436e5413d184de
chnoumanejaz <noumanejaz92@gmail.com>
2024-03-30 16:26:44 +0500

    img tag changes to Image

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,14 +1,16 @@
-/* eslint-disable @next/next/no-img-element */
 import { Product } from '@/app/db';
+import Image from 'next/image';
 import React from 'react';
 
 const ProductCard = ({ product }: { product: Product }) => {
   return (
     <div className="group relative">
       <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80 transition-opacity cursor-pointer">
-        <img
+        <Image
           src={product.imageId}
           alt={product.name}
+          width={500}
+          height={500}
           className="h-full w-full object-cover object-center"
         />
       </div>
